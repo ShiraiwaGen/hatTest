d85f1dae09277a65a1fee845dabd63786fa06dd4
t.sakuma <t.sakuma@twinfeather.co.jp>
2022-09-14 15:38:52 +0900

    申込入力画面追加

---
+++ Dairiten/Pages/Moshikominyuryoku.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Dairiten.Pages
+{
+    public class MoshikominyuryokuModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
