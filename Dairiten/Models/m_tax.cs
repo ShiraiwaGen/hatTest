cb3ec67c581807d7f57179dd33acdfc368af7085
t.sakuma <t.sakuma@twinfeather.co.jp>
2022-09-22 11:46:12 +0900

    Moshikomi_Kにファイル名変更

--- Dairiten/Models/m_tax.cs
+++ Dairiten/Models/m_tax.cs
@@ -13,7 +13,7 @@ namespace Dairiten.Models
         [DataType(DataType.Date)]
         public DateTime shuryobi { get; set; }
         [DisplayName("消費税")]
-        [RegularExpression(@"^\d+(\.\d{1,2})?$", ErrorMessage = "少数以下２桁以上は入力できません"]
+        [RegularExpression(@"^\d+(\.\d{1,2})?$", ErrorMessage = "少数以下２桁以上は入力できません")]
         public decimal tax { get; set; }
     }
 }
