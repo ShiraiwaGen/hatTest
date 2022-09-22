e9f45caabf5161567338713d87ef1693f3c28521
a-hasegawa-tfc <106725782+a-hasegawa-tfc@users.noreply.github.com>
2022-09-22 17:36:35 +0900

    DbContext

--- Dairiten/Data/ApplicationDbContext.cs
+++ Dairiten/Data/ApplicationDbContext.cs
@@ -1,7 +1,6 @@
 ﻿using Dairiten.Models;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
-using Dairiten.Models;
 
 namespace Dairiten.Data
 {
@@ -12,12 +11,18 @@ namespace Dairiten.Data
         {
         }
 
+        public DbSet<Dairiten.Models.m_company> m_company { get; set; }
+        public DbSet<Dairiten.Models.m_shohin> m_shohin { get; set; }
+        public DbSet<Dairiten.Models.m_dairiten> m_dairiten { get; set; }
         public DbSet<Dairiten.Models.t_keiyaku> t_keiyaku { get; set; }
-
+        
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
-            modelBuilder.Entity<t_keiyaku>().ToTable("t_keiyaku");
+            modelBuilder.Entity<m_company>().ToTable("m_company");
+            modelBuilder.Entity<m_shohin>().ToTable("m_shohin");
+            modelBuilder.Entity<m_dairiten>().ToTable("m_dairiten");
+            modelBuilder.Entity<t_keiyaku>().ToTable("t_keiyaku");            
         }
     }
 }
