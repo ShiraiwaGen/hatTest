32f67a31f65336cef9ef31c2c570730c089ca7a4
t.sakuma <t.sakuma@twinfeather.co.jp>
2022-10-05 15:37:18 +0900

    申込検索画面編集

--- Dairiten/wwwroot/js/site.js
+++ Dairiten/wwwroot/js/site.js
@@ -1,7 +1,7 @@
 ﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
 
-import { Modal } from "../lib/bootstrap/dist/js/bootstrap.bundle";
+//import { Modal } from "../lib/bootstrap/dist/js/bootstrap.bundle";
 
 // Write your JavaScript code.
 
